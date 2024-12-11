189714f1478fe0f1737a74d427d1c4bf0075381b
Adham Mohamed Sabry Zineldin <mohalya3@gmail.com>
2024-12-11 13:32:26 +0200

    Registration Created

--- src/main/java/org/eventmanagmentsystem/models/Admin.java
+++ src/main/java/org/eventmanagmentsystem/models/Admin.java
@@ -1,6 +1,7 @@
 package org.eventmanagmentsystem.models;
 
 public class Admin extends User {
+
     public Admin(int id, String userName, String email, String role) {
         super(id, userName, email, role);
     }
