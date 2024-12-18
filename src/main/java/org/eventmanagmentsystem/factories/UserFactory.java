c74f95913ed0e409252437653e27aed877537b8b
Adham Mohamed Sabry Zineldin <mohalya3@gmail.com>
2024-12-18 21:17:52 +0200

    solved timeline2

--- src/main/java/org/eventmanagmentsystem/factories/UserFactory.java
+++ src/main/java/org/eventmanagmentsystem/factories/UserFactory.java
@@ -5,11 +5,11 @@ import org.eventmanagmentsystem.models.*;
 public class UserFactory {
 
     public static User createUser(int id, String username, String password, String email, String role) {
-        return switch (role) {
+        return switch (role.toLowerCase()) {
             case "admin" -> new Admin(id, username,password, email, role);
             case "customer" -> new Customer(id, username,password, email, role);
             case "manager" -> new ProjectManager(id, username,password, email, role);
-            case "serviceProvider" -> new ServiceProvider(id, username, password, email, role);
+            case "serviceprovider" -> new ServiceProvider(id, username, password, email, role);
             case null, default -> throw new IllegalArgumentException("Invalid role: " + role);
         };
     }
